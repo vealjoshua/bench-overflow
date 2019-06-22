817d0a487ba3cdc12824b3a48919e13e2b9fb0e9
Joshua Veal-Briscoe <jkv2c9@mail.umsl.edu>
2019-06-21 20:41:44 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from './Components/Home/Home'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+
+const App: React.FC = () => {
+	return (
+		<BrowserRouter>
+			<Header />
+			<Route path='/' render={() => <Home />} />
+			<Footer />
+		</BrowserRouter>
+	)
+}
+
+export default App
