cf64e24045cc064f95499081465d294088f5fe87
Joshua Veal-Briscoe <jkv2c9@mail.umsl.edu>
2019-06-23 09:35:48 -0500

    wip

--- src/Components/QuestionPage/QuestionView/QuestionView.tsx
+++ src/Components/QuestionPage/QuestionView/QuestionView.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { RootState } from '../../../Redux'
 import { connect } from 'react-redux'
 import { questionActions, SetActiveQuestionAction } from '../../../Redux/Actions/questionActions'
-import { Question } from '../../../Redux/Types/Question'
+import { Question, Reply } from '../../../Redux/Types/Question'
 
 export interface QuestionViewProps {
 	activeQuestion: Question | null
@@ -12,37 +12,53 @@ export interface QuestionViewProps {
 const QuestionView: React.FunctionComponent<QuestionViewProps> = props => {
 	const { activeQuestion } = props
 
-	return (
-		<>
-			<div className='card'>
-				<div className='card-header'>{activeQuestion && activeQuestion.question}</div>
-				<div className='card-body'>
-					<p className='card-text'>{activeQuestion && activeQuestion.description}</p>
-					<a href='#' className='btn btn-primary'>
-						Go somewhere
-					</a>
+	const comments =
+		activeQuestion &&
+		activeQuestion.comments.map((comment: Reply) => (
+			<div>
+				<hr />
+				<div className='card mb-3'>
+					<div className='row no-gutters'>
+						<div className='col-md-10'>
+							<div className='card-body'>
+								<p className='card-text'>{comment.answer}</p>
+								<p className='card-text'>
+									<small className='text-muted'>Last updated 3 mins ago</small>
+								</p>
+							</div>
+						</div>
+						<div className='col-md-2'>
+							<img
+								src='https://media.licdn.com/dms/image/C4E03AQFzD6ERpy88cw/profile-displayphoto-shrink_800_800/0?e=1567036800&v=beta&t=mS8Cz9Wmqy25DWr2a0NgK7XAMnw0uYFQnH-KJxatAhY'
+								className='card-img'
+								alt='...'
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
-			<hr />
+		))
+
+	return (
+		<>
 			<div className='card mb-3'>
+				<div className='card-header text-white bg-primary font-weight-bold'>
+					{activeQuestion && activeQuestion.question}
+				</div>
 				<div className='row no-gutters'>
-					<div className='col-md-4'>
+					<div className='col-md-2'>
 						<img
 							src='https://media.licdn.com/dms/image/C4E03AQFzD6ERpy88cw/profile-displayphoto-shrink_800_800/0?e=1567036800&v=beta&t=mS8Cz9Wmqy25DWr2a0NgK7XAMnw0uYFQnH-KJxatAhY'
 							className='card-img'
 							alt='...'
 						/>
 					</div>
-					<div className='col-md-8'>
-						<div className='card-body'>
-							<p className='card-text'>Hello I am Tom! Pee pee poo poo bpbaoaba opranper aia n</p>
-							<p className='card-text'>
-								<small className='text-muted'>Last updated 3 mins ago</small>
-							</p>
-						</div>
+					<div className='card-body'>
+						<p className='card-text'>{activeQuestion && activeQuestion.description}</p>
 					</div>
 				</div>
 			</div>
+			{comments}
 		</>
 	)
 }
